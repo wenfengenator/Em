7dace403eabd5607f6a47bf45e02c4429d9b1dbb
Wenfeng Liu <wenfeng.liu@microsoft.com>
2015-07-11 21:57:08 -0700

    License update

--- emcore/EmNonCopyable.h
+++ emcore/EmNonCopyable.h
@@ -1,11 +1,24 @@
-/* * This file is part of Em Package * *
-    Copyright © 2015 Em and/or its subsidiary(-ies). All rights reserved. *
-                    Contact: Wenfeng Liu <wenfengenator@gmail.com> * *
-    This software, including documentation, is protected by copyright controlled by Em.
-    All rights are reserved. Copying, including reproducing, storing, adapting or translating, any or
-    all of this material requires the prior written consent of Microsoft Mobile Oy. This material also
-    contains confidential information which may not be disclosed to others * without the prior written
-    consent of Em. */
+/* 
+Copyright (c) 2015, by Wenfeng Liu, wenfengenator@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 
 #pragma once
 
@@ -24,4 +37,4 @@ namespace Em
         NonCopyable(const NonCopyable&);
         const NonCopyable& operator=(const NonCopyable&);
     };
-} // namespace Em
+}
